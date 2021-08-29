850e503165f9dc1f498ca44654a4f927f6ec389b
tgremlin <allantenison@live.com>
2021-08-29 16:29:40 -0500

    Adding code

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ resource "aws_vpc" "main" {
     Name = "main"
   }
 }
+
+resource "aws_subnet" "private" {
+  for_each = var.azs
+
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = format("%s%s", local.region_name, each.key)
+  cidr_block              = cidrsubnet(var.cidr_block, 8, each.value)
+  map_public_ip_on_launch = false
+}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,12 @@ variable "vpc_cidr" {
   type = string
   default = "10.0.0.0/16"
 }
+
+variable "azs" {
+ type = map(number)
+ default = {
+   a = 1
+   b = 2
+   c = 3
+ }
+}
