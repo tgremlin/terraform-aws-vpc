81ae598cb58eb06428e20bde4fefe959186d754a
tgremlin <allantenison@live.com>
2021-08-29 18:56:11 -0500

    Updating to local values

--- variables.tf
+++ variables.tf
@@ -1,13 +1,50 @@
-variable "vpc_cidr" {
-  type = string
-  default = "10.0.0.0/16"
+variable "cidr_ab" {
+    type = map
+    default = {
+        development     = "172.22"
+        qa              = "172.24"
+        staging         = "172.26"
+        production      = "172.28"
+    }
 }
 
-variable "azs" {
- type = map(number)
- default = {
-   a = 1
-   b = 2
-   c = 3
- }
+locals {
+    cidr_c_private_subnets  = 1
+    cidr_c_database_subnets = 11
+    cidr_c_public_subnets   = 64
+
+    max_private_subnets     = 2
+    max_database_subnets    = 2
+    max_public_subnets      = 2
+}
+
+data "aws_availability_zones" "available" {
+    state = "available"
+}
+
+locals {
+    availability_zones = data.aws_availability_zones.available.names
+}
+
+variable "environment" {
+    type = string
+    description = "Options: development, qa, staging, production"
+}
+
+locals {
+    private_subnets = [
+        for az in local.availability_zones : 
+            "${lookup(var.cidr_ab, var.environment)}.${local.cidr_c_private_subnets + index(local.availability_zones, az)}.0/24"
+            if index(local.availability_zones, az) < local.max_private_subnets
+        ]
+    database_subnets = [
+        for az in local.availability_zones : 
+            "${lookup(var.cidr_ab, var.environment)}.${local.cidr_c_database_subnets + index(local.availability_zones, az)}.0/24"
+            if index(local.availability_zones, az) < local.max_database_subnets
+        ]
+    public_subnets = [
+        for az in local.availability_zones : 
+            "${lookup(var.cidr_ab, var.environment)}.${local.cidr_c_public_subnets + index(local.availability_zones, az)}.0/24"
+            if index(local.availability_zones, az) < local.max_public_subnets
+        ]
 }
