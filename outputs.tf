413467aa33c581484c8c971ceb46dcf22f8c5b85
tgremlin <allantenison@live.com>
2021-08-29 21:32:05 -0500

    added public/database subnet outputs and azs

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,15 @@ output "vpc_id" {
 output "private_subnets" {
   value = module.vpc.private_subnets[*].id
 }
+
+output "public_subnets" {
+  value = module.vpc.public_subnets[*].id
+}
+
+output "database_subnets" {
+  value = module.vpc.database_subnets[*].id
+}
+
+output "azs" {
+  value = module.vpc.azs[*].id
+}
